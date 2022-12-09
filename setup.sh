1fcd1336774686194a5f01780701b9bc3d0e793d
atreyaray@gmail.com <atreyaray@gmail.com>
2022-12-09 18:47:48 +0200

    added heroku config

--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
-#!/bin/bash  
-python3 -m venv venv 
-source venv/bin/activate
-pip install -r requirements.txt
-
-mkdir Dataset && mkdir Preprocessed\ Dataset
-cd Dataset
-# if you don't have wget, manually download the zip file from 
-# https://github.com/karoldvl/ESC-50/archive/master.zip and put it in the Dataset folder
-wget https://github.com/karoldvl/ESC-50/archive/master.zip
-unzip master.zip
-rm master.zip
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
